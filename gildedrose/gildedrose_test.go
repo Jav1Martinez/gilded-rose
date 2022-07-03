cc92cc088c0926b82ae43b3e8eee6ede153ccc8b
Javier Martinez Abarca <javier.martinezabarca@codurance.com>
2022-07-03 20:19:23 +0200

    First step - Test coverage initialization.
    - Quality not decreased if current value is 0 unit test.

--- gildedrose/gildedrose_test.go
+++ gildedrose/gildedrose_test.go
@@ -35,4 +35,13 @@ func Test_Gildedrose(t *testing.T) {
 		gildedrose.UpdateQuality(items)
 		assert.Equal(t, 8, items[0].Quality)
 	})
+
+	t.Run("Update quality execution should not decrease items Quality if the current value is 0", func(t *testing.T) {
+
+		var items = []*gildedrose.Item{
+			{"foo", 0, 0},
+		}
+		gildedrose.UpdateQuality(items)
+		assert.Equal(t, 0, items[0].Quality)
+	})
 }
