80b4284bc4f72b434c7e7baa48e5bb7363151229
Javier Martinez Abarca <javier.martinezabarca@codurance.com>
2022-07-03 21:00:13 +0200

    First step - Test coverage initialization.
    - Unit tests refactorization.

--- gildedrose/gildedrose_test.go
+++ gildedrose/gildedrose_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
 )
 
-func Test_Gildedrose(t *testing.T) {
+func Test_Item_SellIn_Gildedrose(t *testing.T) {
 
 	t.Run("Update quality execution should decrease items SellIn value", func(t *testing.T) {
 
@@ -17,67 +17,56 @@ func Test_Gildedrose(t *testing.T) {
 		gildedrose.UpdateQuality(items)
 		assert.Equal(t, 9, items[0].SellIn)
 	})
+}
 
-	t.Run("Update quality execution should decrease items Quality value", func(t *testing.T) {
+func Test_Item_Quality_Gildedrose(t *testing.T) {
 
-		var items = []*gildedrose.Item{
-			{"foo", 10, 10},
-		}
-		gildedrose.UpdateQuality(items)
-		assert.Equal(t, 9, items[0].Quality)
-	})
+	quality_tests := []struct {
+		name     string
+		args     []*gildedrose.Item
+		expected int
+	}{
+		{
+			name:     "Update quality execution should decrease items Quality value",
+			args:     []*gildedrose.Item{{"foo", 10, 10}},
+			expected: 9,
+		},
+		{
+			name:     "Update quality execution should decrease items Quality value 2x faster after sell date has passed",
+			args:     []*gildedrose.Item{{"foo", 0, 10}},
+			expected: 8,
+		},
+		{
+			name:     "Update quality execution should not decrease items Quality if the current value is 0",
+			args:     []*gildedrose.Item{{"foo", 0, 0}},
+			expected: 0,
+		},
+		{
+			name:     "Update quality execution should increase Aged Brie quality in 1 point by day if SellIn is higher than 0",
+			args:     []*gildedrose.Item{{"Aged Brie", 1, 10}},
+			expected: 11,
+		},
+		{
+			name:     "Update quality execution should increase Aged Brie quality in 2 point by day if SellIn is 0 or lower",
+			args:     []*gildedrose.Item{{"Aged Brie", 0, 10}},
+			expected: 12,
+		},
+		{
+			name:     "Update quality execution should not increase Aged Brie item quality value beyond 50",
+			args:     []*gildedrose.Item{{"Aged Brie", 10, 50}},
+			expected: 50,
+		},
+		{
+			name:     "Update quality execution should not increase Aged Brie item quality value beyond 50 even if sellIn value is 0 or lower",
+			args:     []*gildedrose.Item{{"Aged Brie", 0, 50}},
+			expected: 50,
+		},
+	}
 
-	t.Run("Update quality execution should decrease items Quality value 2x faster after sell date has passed", func(t *testing.T) {
-
-		var items = []*gildedrose.Item{
-			{"foo", 0, 10},
-		}
-		gildedrose.UpdateQuality(items)
-		assert.Equal(t, 8, items[0].Quality)
-	})
-
-	t.Run("Update quality execution should not decrease items Quality if the current value is 0", func(t *testing.T) {
-
-		var items = []*gildedrose.Item{
-			{"foo", 0, 0},
-		}
-		gildedrose.UpdateQuality(items)
-		assert.Equal(t, 0, items[0].Quality)
-	})
-
-	t.Run("Update quality execution should increase Aged Brie quality in 1 point by day if SellIn is higher than 0", func(t *testing.T) {
-
-		var items = []*gildedrose.Item{
-			{"Aged Brie", 1, 10},
-		}
-		gildedrose.UpdateQuality(items)
-		assert.Equal(t, 11, items[0].Quality)
-	})
-
-	t.Run("Update quality execution should increase Aged Brie quality in 2 point by day if SellIn is 0 or lower", func(t *testing.T) {
-
-		var items = []*gildedrose.Item{
-			{"Aged Brie", 0, 10},
-		}
-		gildedrose.UpdateQuality(items)
-		assert.Equal(t, 12, items[0].Quality)
-	})
-
-	t.Run("Update quality execution should not increase Aged Brie item quality value beyond 50", func(t *testing.T) {
-
-		var items = []*gildedrose.Item{
-			{"Aged Brie", 10, 50},
-		}
-		gildedrose.UpdateQuality(items)
-		assert.Equal(t, 50, items[0].Quality)
-	})
-
-	t.Run("Update quality execution should not increase Aged Brie item quality value beyond 50 even if sellIn value is 0 or lower", func(t *testing.T) {
-
-		var items = []*gildedrose.Item{
-			{"Aged Brie", 0, 50},
-		}
-		gildedrose.UpdateQuality(items)
-		assert.Equal(t, 50, items[0].Quality)
-	})
+	for _, test := range quality_tests {
+		t.Run(test.name, func(t *testing.T) {
+			gildedrose.UpdateQuality(test.args)
+			assert.Equal(t, test.expected, test.args[0].Quality)
+		})
+	}
 }
